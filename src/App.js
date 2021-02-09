85b1d72c1674148ea03d42b4da4d63f36c97a40f
Patrick Kaipainen <patrick.kaipainen@gmail.com>
2021-02-09 14:43:44 -0500

    removed some old console.logs

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ import CallCardList from './Components/CallCardList/CallCardList';
 class App extends Component {
   constructor() {
     super();
-    this.state = {      
+    this.state = {
       datasets: {},
       start: "2020-12-05",
       end: "2020-12-31",
-      company: "",            
+      company: "",
       colors: [],
       callCardData: [],
-      searchfield: ""           
+      searchfield: ""
     }
   }
 
@@ -42,7 +42,7 @@ class App extends Component {
     U: "#795548",
   }
 
-  handlePickerSize = () => {    
+  handlePickerSize = () => {
     const picker = document.querySelector('.ClassificationPicker');
     const currentHeight = picker.offsetHeight;
     if (currentHeight > 34) {
@@ -58,13 +58,13 @@ class App extends Component {
     // pass this in to the picker component
     // the picker onChange hook will pass to it
     // 3 arguments
-    // selectedDates, dateStr, instance    
+    // selectedDates, dateStr, instance
   }
 
-  onButtonSubmit = () => {    
+  onButtonSubmit = () => {
     const start = document.querySelector('#startPicker').value;
     const end = document.querySelector('#endPicker').value;
-    const company = document.querySelector('#companyInput').value;   
+    const company = document.querySelector('#companyInput').value;
 
     // validate date inputs
     // convert to date objects and compare to make sure end is later than start
@@ -74,29 +74,29 @@ class App extends Component {
     this.setState({start, end, company})
 
     // validate company input
-    // only permit alphanumeric chars    
+    // only permit alphanumeric chars
     const pattern = /^[a-zA-Z0-9 ]*$/
-    if (!pattern.test(company)) {      
+    if (!pattern.test(company)) {
       return alert("Invalid characters in company input");
     }
 
     const checkboxes = document.querySelectorAll('.ClassificationPickerCheckbox');
     let clicked = [];
-    checkboxes.forEach(box => {      
+    checkboxes.forEach(box => {
       if (box.checked) clicked.push(box.value);
     });
-    // if (clicked.length === 0) clicked = ["JW", "AW"];    
+    // if (clicked.length === 0) clicked = ["JW", "AW"];
     this.getData(clicked, start, end, company);
     this.getCallSheetData(clicked, start, end, company);
-    setTimeout(this.handlePickerSize,500);       
+    setTimeout(this.handlePickerSize,500);
   }
 
   onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });   
-  }  
-  
-  prepareDatasets = (response) => {    
-    const keys = Object.keys(response[0]);       
+    this.setState({ searchfield: event.target.value });
+  }
+
+  prepareDatasets = (response) => {
+    const keys = Object.keys(response[0]);
     const datasets = {};
     keys.forEach(key => datasets[key] = []);
     response.forEach(obj => {
@@ -104,20 +104,20 @@ class App extends Component {
         datasets[key].push(obj[key]);
       }
     })
-    return datasets;       
+    return datasets;
   }
 
   getData = (clicked, start, end, company) => {
     const body = {
       "start": start || "2020-12-04",
       "end": end || "2020-12-31",
-      // "member_class": clicked,        
+      // "member_class": clicked,
     }
-    if (clicked?.length) {      
+    if (clicked?.length) {
       body.member_class = clicked;
     }
 
-    if (company?.length) {      
+    if (company?.length) {
       body.company = company;
     }
     //http://127.0.0.1:3000/ for local db
@@ -126,15 +126,15 @@ class App extends Component {
       headers: {'Content-type': 'application/json'},
       body: JSON.stringify(body)
     })
-    .then(response => response.json())   
+    .then(response => response.json())
     .then(response => {
-      // console.log("this came back from the API: ", response);         
+      // console.log("this came back from the API: ", response);
      return this.prepareDatasets(response);
-    })   
+    })
     .then(preparedDatasets => {
       // console.log("prepareDatasets returned: ", preparedDatasets);
       this.setState({datasets: {}})
-      this.setState(Object.assign(this.state.datasets, preparedDatasets));     
+      this.setState(Object.assign(this.state.datasets, preparedDatasets));
     })
     .catch(err => console.log("Fetch failed ", err))
   }
@@ -145,10 +145,10 @@ class App extends Component {
       "end": end || "2020-12-31",
       // "member_class": clicked
     }
-    if (clicked?.length) {      
+    if (clicked?.length) {
       body.member_class = clicked;
-    }    
-    if (company?.length) {      
+    }
+    if (company?.length) {
       body.company = company;
     }
     //http://127.0.0.1:3000/ for local db
@@ -157,20 +157,20 @@ class App extends Component {
       headers: {'Content-type': 'application/json'},
       body: JSON.stringify(body)
     })
-    .then(response => response.json())   
+    .then(response => response.json())
     .then(response => {
       // console.log("here's the stuff: ", response);
-      this.setState({callCardData: []})  
+      this.setState({callCardData: []})
       this.setState(Object.assign(this.state.callCardData, response));
     })
   }
 
-  componentDidMount() {    
+  componentDidMount() {
     this.getData();
-    this.getCallSheetData();    
+    this.getCallSheetData();
   }
 
-  render() {    
+  render() {
     const { callCardData, searchfield} = this.state;
     const filteredCalls = callCardData.filter(call => {
       return call.summary.toLowerCase().includes(searchfield.toLowerCase());
@@ -178,7 +178,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="layoutMaster">
-        <div id="header">      
+        <div id="header">
           <h1>IBEW LOCAL 353</h1>
           <h1>JOB CALLS DATABASE</h1>
         </div>
@@ -190,41 +190,41 @@ class App extends Component {
             handlePickerSize={this.handlePickerSize}
           />
           <div className="graphMaster">
-            <StartEndDates 
+            <StartEndDates
               start={this.state.start}
               end={this.state.end}
               company={this.state.company}
             />
-            <div className="multiGraphContainer">              
-              <LineGraph 
-                datasets={this.state.datasets}                
-                colors={this.colors} 
-              />
-            </div>              
             <div className="multiGraphContainer">
-              <TotalLineGraph
-                datasets={this.state.datasets} 
-              />      
-              <DoughnutGraph
+              <LineGraph
                 datasets={this.state.datasets}
-                colors={this.colors} 
+                colors={this.colors}
               />
             </div>
-            <ColorLegend datasets={this.state.datasets} colors={this.colors} /> 
+            <div className="multiGraphContainer">
+              <TotalLineGraph
+                datasets={this.state.datasets}
+              />
+              <DoughnutGraph
+                datasets={this.state.datasets}
+                colors={this.colors}
+              />
+            </div>
+            <ColorLegend datasets={this.state.datasets} colors={this.colors} />
           </div>
-          {/*<SearchBox 
+          {/*<SearchBox
             searchChange={this.onSearchChange}
             count={filteredCalls.length}
           />*/}
-          <CallCardList 
+          <CallCardList
             callCardData={filteredCalls}
             colors={this.colors}
-            searchChange={this.onSearchChange}            
+            searchChange={this.onSearchChange}
           />
-        </div>        
+        </div>
       </div>
     );
-  }  
+  }
 }
 
 export default App;
--- src/Components/StartEndDates/StartEndDates.js
+++ src/Components/StartEndDates/StartEndDates.js
@@ -1,23 +1,21 @@
 import React from 'react';
 
 const StartEndDates = (props) => {
-		console.log(props?.company);
-		console.log(props.company.length);
 	const humanDate = (iso_date_string) => {
 			const myString = new Date(iso_date_string).toUTCString();
 			return myString.replace(/ \d\d:\d\d:\d\d \w\w\w/, "");
-	}	
+	}
 	return (
 		<div id="StartEndDates">
 			{
 				(function(){
-					return props.company.length? 
+					return props.company.length?
 						<h3>Results Matching '{props.company}'</h3>
 						:
 						<h3>Results For All Companies</h3>
 				}())
 			}
-			<h3>									
+			<h3>
 				{humanDate(props.start)}
 				{' to '}
 				{humanDate(props.end)}
